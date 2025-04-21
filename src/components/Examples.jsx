b887082f27e69a1425461f08f314df28d3080107
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-04-22 04:04:19 +0600

    fix some bug

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -27,6 +27,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        buttonContainer="menu"
         buttons={
           <>
             <TabButton
